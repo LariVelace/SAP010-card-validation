5d1227512843796cf3908e49f765beaaccfac205
LariVelace <131285148+LariVelace@users.noreply.github.com>
2023-04-20 12:36:52 -0300

    Coloquei função de validação e tema

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+function validarCartao() {
+    var numeroCartao = document.getElementById("cartao").value;
+    alert(numeroCartao);
+}
+
+document.getElementById("verificarCartao").addEventListener("click", function () {
+    return validarCartao();
+});
